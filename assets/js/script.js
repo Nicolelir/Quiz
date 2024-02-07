e8b3e254c0c35a093d1388b73209c70fdee49a2c
Ryan Ferris <r.ferris137@gmail.com>
2024-02-07 19:29:45 +0000

    added intro screen page with javascript and bootstrap layout

--- assets/js/script.js
+++ assets/js/script.js
@@ -114,3 +114,25 @@ function countToThree() {
   }
   
   countToThree();
+
+  // Ryan's Code Below 
+
+  // Code for instructions overlay
+  document.addEventListener("DOMContentLoaded", function() {
+	const openButton = document.getElementById("openButton");
+	const overlay = document.getElementById("overlay");
+
+	openButton.addEventListener("click", function() {
+	  overlay.style.display = "block";
+	  setTimeout(() => {
+		overlay.style.opacity = "1";
+	  }, 10);
+	});
+
+	overlay.addEventListener("click", function() {
+	  overlay.style.opacity = "0";
+	  setTimeout(() => {
+		overlay.style.display = "none";
+	  }, 300); // Adjust the duration to match the transition duration
+	});
+  });
