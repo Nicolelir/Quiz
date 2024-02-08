3957d6fc4ca286101ac4c8d4858944617c72b087
Katie Coughlan <katiejanecoughlan@gmail.com>
2024-02-08 09:48:36 +0000

    Add nicoles code

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,38 +1,16 @@
-
-// --NICOLE--
-
-//random function
-function runGame(gameType) {
-	playerAction = gameType;
-	computerAction = Math.floor(Math.random() * 5);
-	switch (computerAction) {
-		case 0: computerAction = "rock";
-			break;
-		case 1: computerAction = "paper";
-			break;
-		case 2: computerAction = "scissors";
-			break;
-		case 3: computerAction = "lizard";
-			break;
-		case 4: computerAction = "spock";
-			break;
-	}
-
-	var result = compare(computerAction).toUpperCase();
-
-	document.querySelector(".result").innerHTML = "<h3>User/Computer:</h3><p>User: " + playerAction.toUpperCase() +
-		"<br>" + "Computer: " + computerAction.toUpperCase() + "</p>" + "<p>" + result + "</p>";
-}
-
-// 3 rounds function
-document.addEventListener("DOMContentLoaded", function() {
+let userChoice;
+let computerChoice;
+let roundsPlayed = 0;
+const maxRounds = 3;
+let playerAction;
+let computerAction;
+let playerTally = 0;
+let computerTally = 0;
+let round = 0;
+document.addEventListener("DOMContentLoaded", function () {
     let buttons = document.getElementsByTagName("button");
-	let roundsPlayed = 0;
-    const maxRounds = 3;
-
-
-	for (let button of buttons) {
-        button.addEventListener("click", function() {
+    for (let button of buttons) {
+        button.addEventListener("click", function () {
             if (this.getAttribute("data-type") === "submit") {
                 checkAnswer();
             } else {
@@ -41,76 +19,118 @@ document.addEventListener("DOMContentLoaded", function() {
                     runGame(gameType);
                     roundsPlayed++;
                 }
-                
-                if (roundsPlayed === maxRounds) { // Add here function after 3 rounds.... 
-                   
+                if (roundsPlayed === maxRounds) {
+                    displayOverallWinner();
                 }
             }
         });
     }
 });
-
-// -----Katie WIP----
-// Check winner of round
-var playerAction;
-var computerAction;
-
+function runGame(gameType) {
+    playerAction = gameType;
+    computerAction = Math.floor(Math.random() * 5);
+    switch (computerAction) {
+        case 0: computerAction = "rock";
+            break;
+        case 1: computerAction = "paper";
+            break;
+        case 2: computerAction = "scissors";
+            break;
+        case 3: computerAction = "lizard";
+            break;
+        case 4: computerAction = "spock";
+            break;
+    }
+    var result = compare(computerAction).toUpperCase();
+    document.querySelector(".result").innerHTML = "<h3>User/Computer:</h3><p>User: " + playerAction.toUpperCase() +
+        "<br>" + "Computer: " + computerAction.toUpperCase() + "</p>" + "<p>" + result + "</p>";
+    return result; // Return the result for updating scores
+}
 function compare(computerAction) {
-	if (playerAction === comChoice) {
-    return "Snap! It's a draw!";
-	} else if (playerAction === "rock") {
-		if (computerAction === "paper") {
-      return "Bazinga, you lost!";
-		} else if (computerAction === "scissors") {
-      return "Eureka, you won!";
-		} else if (computerAction === "lizard") {
-			return "Eureka, you won!";
-		} else {
-			return "Bazinga, you lost!";
-		}
-	} else if (playerAction === "paper") {
-		if (computerAction === "rock") {
-			return "Eureka, you won!";
-		} else if (computerAction === "scissors") {
-			return "Bazinga, you lost!";
-		} else if (computerAction === "lizard") {
-			return "Bazinga, you lost!";
-		} else {
-			return "Eureka, you won!";
-		}
-	}else if (playerAction === "scissors") {
-		if (computerAction === "rock") {
-			return "Bazinga, you lost!";
-		} else if (computerAction === "paper") {
-			return "Eureka, you won!";
-		} else if (computerAction === "lizard") {
-			return "Eureka, you won!";
-		} else {
-			return "Bazinga, you lost!";
-		}
-	} else if (playerAction === "lizard") {
-		if (computerAction === "rock") {
-			return "Bazinga, you lost!";
-		} else if (computerAction === "paper") {
-			return "Eureka, you won!";
-		} else if (computerAction === "scissors") {
-			return "Bazinga, you lost!";
-		} else {
-			return "Eureka, you won!";
-		}
-	} else if (playerAction === "spock") {
-		if (computerAction === "rock") {
-			return "Eureka, you won!";
-		} else if (computerAction === "paper") {
-			return "Bazinga, you lost!";
-		} else if (computerAction === "scissors") {
-			return "Eureka, you won!";
-		} else {
-			return "Bazinga, you lost!";
-		}
-	}
-<<<<<<< HEAD
-  }
+    if (playerAction === computerAction) {
+        return "Snap! It's a draw!";
+    } else if (playerAction === "rock") {
+        if (computerAction === "paper") {
+            return "Bazinga, you lost!";
+        } else if (computerAction === "scissors") {
+            return "Eureka, you won!";
+        } else if (computerAction === "lizard") {
+            return "Eureka, you won!";
+        } else {
+            return "Bazinga, you lost!";
+        }
+    } else if (playerAction === "paper") {
+        if (computerAction === "rock") {
+            return "Eureka, you won!";
+        } else if (computerAction === "scissors") {
+            return "Bazinga, you lost!";
+        } else if (computerAction === "lizard") {
+            return "Bazinga, you lost!";
+        } else {
+            return "Eureka, you won!";
+        }
+    } else if (playerAction === "scissors") {
+        if (computerAction === "rock") {
+            return "Bazinga, you lost!";
+        } else if (computerAction === "paper") {
+            return "Eureka, you won!";
+        } else if (computerAction === "lizard") {
+            return "Eureka, you won!";
+        } else {
+            return "Bazinga, you lost!";
+        }
+    } else if (playerAction === "lizard") {
+        if (computerAction === "rock") {
+            return "Bazinga, you lost!";
+        } else if (computerAction === "paper") {
+            return "Eureka, you won!";
+        } else if (computerAction === "scissors") {
+            return "Bazinga, you lost!";
+        } else {
+            return "Eureka, you won!";
+        }
+    } else if (playerAction === "spock") {
+        if (computerAction === "rock") {
+            return "Eureka, you won!";
+        } else if (computerAction === "paper") {
+            return "Bazinga, you lost!";
+        } else if (computerAction === "scissors") {
+            return "Eureka, you won!";
+        } else {
+            return "Bazinga, you lost!";
+        }
+    }
+}
+/*function winner() {
+    if (computerTally > playerTally) {
+        return "Bazinga, the computer won the game!";
+    } else if (computerTally === playerTally) {
+        return "It's a draw!"
+    } else {
+        return "Eureka, congrats, you won the game!";
+    }
+}
+*/
+function displayOverallWinner() {
+    if (computerTally > playerTally) {
+        overallResult = "Bazinga, the computer won the game!";
+    } else if (computerTally === playerTally) {
+        overallResult = "It's a draw!";
+    } else {
+        overallResult = "Eureka, congrats, you won the game!";
+    }
+    document.querySelector(".overall-result").innerHTML = "<h2>Overall Result:</h2><p>" + overallResult + "</p>";
+}
+function resultsTally(winner) {
+    round++;
+    if (winner === "player") {
+        playerTally++;
+        document.getElementById("playerTally").innerHTML = playerTally;
+    } else if (winner === "computer") {
+        computerTally++;
+        document.getElementById("computerTally").innerHTML = computerTally;
+    }
+}
   
   countToThree();
 
@@ -135,33 +155,3 @@ function compare(computerAction) {
 	  }, 300); // Adjust the duration to match the transition duration
 	});
   });
-=======
-};
-
-// Check winner of game
-
-function winner() {
-	if (computerTally > playerTally) {
-		return "Bazinga, the computer won the game!";
-	} else if (computerTally === playerTally) {
-		return "It's a draw!"
-	} else {
-		return "Eureka, congrats, you won the game!";
-	}
-}
-
-// Results tally
-
-resultsTally(winner) {
-	round++;
-	if(winner === player) {
-		playerTally++;
-		document.getElementById("playerTally").innerHTML = playerTally;
-	} else if(winner === computer) {
-		computerTally++;
-		document.getElementById("computerTally").innerHTML = computerTally;
-	}
-}
-
-
->>>>>>> 0c5469e455e746b5093fbd2a06a13b01959a8374
