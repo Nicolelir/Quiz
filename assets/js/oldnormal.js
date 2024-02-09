8ff39da4612945e95458823aa00bc3c7dd39ceaa
Katie Coughlan <katiejanecoughlan@gmail.com>
2024-02-09 11:26:02 +0000

    Swap results and normal page

--- assets/js/oldnormal.js
+++ assets/js/oldnormal.js
@@ -8,6 +8,7 @@ let computerAction;
 let playerTally = 0;
 let computerTally = 0;
 let round = 0;
+// const swup = new Swup();
 
 document.addEventListener("DOMContentLoaded", function () {
     let buttons = document.getElementsByTagName("button");
